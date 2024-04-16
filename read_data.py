8fffefa138775655a542b20b55183c747064ddf0
Kadir Erturk <kerturk@4refuel.com>
2024-04-16 10:29:35 -0700

    the KO format

---
+++ read_data.py
@@ -0,0 +1,23 @@
+from obspy import read
+import numpy as np
+import matplotlib.pyplot as plt
+
+# st = read("data/20040127_153300-OZALP-(VAN).M=3.6/BNG.SHZ.KO")
+# st = read("data/20040128_083800-KEMALIYE-(ERZINCAN).M=3.5/BNN.SHZ.KO")
+st = read("data/20040128_083800-KEMALIYE-(ERZINCAN).M=3.5/ERZ.SHZ.KO")
+station = st[0].stats.station
+sta = st[0].stats.starttime
+end = st[0].stats.endtime
+chn = st[0].stats.channel
+print(sta)
+print(end)
+print(st[0].stats)
+
+data = st[0].data
+
+arr = np.array(data, dtype='int')
+plt.plot(arr)
+plt.show()
+
+print(arr)
+print(len(data))
